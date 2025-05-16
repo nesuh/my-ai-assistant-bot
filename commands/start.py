5f4c53be1f645d5b7a4fa13e792a9c961f0a98e6
nesuh <antenhesileshi@gmail.com>
2025-05-16 20:46:41 +0300

    feat(version01):test05

--- commands/start.py
+++ commands/start.py
@@ -2,4 +2,4 @@ from telegram import Update
 from telegram.ext import ContextTypes
 
 async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
-    await update.message.reply_text("👋 Hello! I’m your Portfolio Bot. Type /help to see what I can do!")
+    await update.message.reply_text("👋 Hello! I’m your Portfolio Bot. Typ/help to see what I can do!")
